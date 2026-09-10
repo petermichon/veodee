4f3fdf6917194732ea24a131f76dae0f6327258b
Peter Michon <147183503+petermichon@users.noreply.github.com>
2026-09-10 22:04:43 +0200

    fix: empty stale files from dist on build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
   },
   build: {
     outDir: path.resolve(import.meta.dirname, '../dist'),
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks(id) {
